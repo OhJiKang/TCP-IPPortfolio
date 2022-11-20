b21d1734f8a5df4a3a932dca96ff03ceb7c69cf8
Quang Thinh <thinhdhqb@gmail.com>
2022-11-20 19:01:33 +0700

    linh tinh

--- src/Component/BuildProfession.js
+++ src/Component/BuildProfession.js
@@ -10,23 +10,14 @@ import RetoucherImg from "../Asset/Retoucher.png";
 import StoreImg from "../Asset/Store.png";
 import StudentImg from "../Asset/Student.png";
 import OtherImg from "../Asset/Other.png";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-function ProfessionBox({ name, img }) {
-  const label = useRef(0);
+function ProfessionBox({ name, img, onClick }) {
 
-  const handleMouseOver = (e) => {
-    label.current.classList.add("invisible", "opacity-0");
-  };
-
-  const handleMouseOut = (e) => {
-    label.current.classList.remove("invisible", "opacity-0");
-  };
   return (
-    <div className="relative w-[180px] h-[180px] group cursor-pointer">
+    <div onClick={onClick} className="relative w-[180px] h-[180px] group cursor-pointer">
       <img className="group-hover:brightness-75" src={img} />
       <div
-        ref={label}
         className="h-[40px] group-hover:opacity-100 opacity-0 transition-opacity ease-in-out bg-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white flex justify-center items-center p-3 rounded-[10px]"
       >
         {name}
@@ -36,24 +27,30 @@ function ProfessionBox({ name, img }) {
 }
 
 function BuildProfession() {
+  const [chosen, setChosen] = useState("");
+
+  const handleClick = (e) => {
+    console.log(e);
+  }
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       <div className="w-[480px] text-center font-heading font-[600] text-[40px] absolute top-28">
         WHAT IS YOUR PROFESSION ?
       </div>
       <div className="grid grid-cols-6 gap-[20px]">
-        <ProfessionBox img={ArchitectImg} name="Architect" />
-        <ProfessionBox img={ArtistImg} name="Artist" />
-        <ProfessionBox img={DesignerImg} name="Designer" />
-        <ProfessionBox img={MakeupArtistImg} name="Makeup" />
-        <ProfessionBox img={ModelImg} name="Model" />
-        <ProfessionBox img={MusicianImg} name="Musician" />
-        <ProfessionBox img={PhotographerImg} name="Photographer" />
-        <ProfessionBox img={ProgrammerImg} name="Programmer" />
-        <ProfessionBox img={RetoucherImg} name="Retoucher" />
-        <ProfessionBox img={StoreImg} name="Store" />
-        <ProfessionBox img={StudentImg} name="Student" />
-        <ProfessionBox img={OtherImg} name="Other" />
+        <ProfessionBox onClick={handleClick} img={ArchitectImg} name="Architect" />
+        <ProfessionBox onClick={handleClick} img={ArtistImg} name="Artist" />
+        <ProfessionBox onClick={handleClick} img={DesignerImg} name="Designer" />
+        <ProfessionBox onClick={handleClick} img={MakeupArtistImg} name="Makeup" />
+        <ProfessionBox onClick={handleClick} img={ModelImg} name="Model" />
+        <ProfessionBox onClick={handleClick} img={MusicianImg} name="Musician" />
+        <ProfessionBox onClick={handleClick} img={PhotographerImg} name="Photographer" />
+        <ProfessionBox onClick={handleClick} img={ProgrammerImg} name="Programmer" />
+        <ProfessionBox onClick={handleClick} img={RetoucherImg} name="Retoucher" />
+        <ProfessionBox onClick={handleClick} img={StoreImg} name="Store" />
+        <ProfessionBox onClick={handleClick} img={StudentImg} name="Student" />
+        <ProfessionBox onClick={handleClick} img={OtherImg} name="Other" />
       </div>
     </div>
   );
